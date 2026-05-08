297a436c95f959c4d5e4e4fbde704721b0ec35c8
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-08 10:15:21 -0300

    admin

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import * #imporata nossos models
-admin.site.register(Fabricante) #adiciona a interface do adm
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Produto)
 class FabricanteAdmin(admin.ModelAdmin):
     # Cria um filtro de hierarquia com datas
     date_hierarchy = 'criado_em'
