e112a958b355a762fc08280a1a81d9bc186efea1
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-08 09:37:09 -0300

    alteração admin.py

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import * #imporata nossos models
 admin.site.register(Fabricante) #adiciona a interface do adm
+from django.contrib import admin
+# Register your models here.
+from .models import *
+admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
