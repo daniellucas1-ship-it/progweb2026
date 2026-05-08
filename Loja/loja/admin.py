9819c4b69da86af3aed15dcbf28c128e796e7275
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-08 10:20:54 -0300

    admin#

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -5,11 +5,11 @@ from .models import * #imporata nossos models
 class FabricanteAdmin(admin.ModelAdmin):
     # Cria um filtro de hierarquia com datas
     date_hierarchy = 'criado_em'
-admin.site.register(Produto, ProdutoAdmin)
-admin.site.register(Categoria)
-admin.site.register(Produto)
 
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
     list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
     empty_value_display = 'Vazio'
+admin.site.register(Fabricante, FabricanteAdmin)
+admin.site.register(Categoria)
+admin.site.register(Produto, ProdutoAdmin)
