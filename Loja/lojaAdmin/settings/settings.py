4e7403f337f0b7f9f9b1ad5640b1d8df80e6056d
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-08 09:54:04 -0300

    altera settings

--- Loja/lojaAdmin/settings/settings.py
+++ Loja/lojaAdmin/settings/settings.py
@@ -119,14 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-USE_I18N = True
-
-USE_TZ = True
