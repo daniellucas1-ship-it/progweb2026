f2d576d1b9a2d38327ec933bf831d079250eaff3
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-11 09:53:41 -0300

    id=0

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -10,6 +10,5 @@ def list_produto_view(request, id=None):
     produtos = Produto.objects.first()
     print(produtos)
     if id is None:
-
-        return HttpResponse('<h1>nenhum id foi informado</h1>')
-return  HttpResponse('<h1>Produto de id %s!</h1>' % id)
+        id = 0
+    return  HttpResponse('<h1>Produto de id %s!</h1>' % id)
