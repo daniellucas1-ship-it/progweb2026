341df41c47af58b399922d4ab62689728e7848c9
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-05-25 10:24:27 -0300

    .

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -2,6 +2,15 @@ from django.shortcuts import render, redirect # Retire from django.http import H
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+def details_produto_view(request, id=None):
+# Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
     if id is not None:
