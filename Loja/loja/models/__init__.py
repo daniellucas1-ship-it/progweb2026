e1b1afe2a74cc6a0bfa8a001ec6649aa511b3e57
daniellucas1 <daniel.lucas1@escolar.ifrn.edu.br>
2026-06-22 09:30:26 -0300

    criação do usuário

--- Loja/loja/models/__init__.py
+++ Loja/loja/models/__init__.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-#acima são bibliotecas padrões necessárias do Django, e abaixo nossos models
-from .Fabricante import Fabricante
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-#acima são bibliotecas padrões necessárias do Django, e abaixo nossos
-models
 from .Fabricante import Fabricante
 from .Categoria import Categoria
 from .Produto import Produto
+# inclua o modelo de usuário
+PERFIL = (
+    (1, 'Admin'),
+    (2, 'Usuario')
+)
+from .Usuario import Usuario
